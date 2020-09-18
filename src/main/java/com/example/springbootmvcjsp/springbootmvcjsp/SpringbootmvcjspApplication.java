be96f345af06d07f8d9211b3780ec2f47487ad93
Bilal Tekin <bt.ce21@gmail.com>
2020-09-18 23:51:50 +0300

    initial commit

---
+++ src/main/java/com/example/springbootmvcjsp/springbootmvcjsp/SpringbootmvcjspApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcjsp.springbootmvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcjspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmvcjspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmvcjsp/springbootmvcjsp/SpringbootmvcjspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootmvcjsp.springbootmvcjsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmvcjspApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
